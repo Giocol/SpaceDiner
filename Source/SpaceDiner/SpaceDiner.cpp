d84485679723b9f4a251b77277315c40db74e094
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-04-11 13:52:21 +0200

    initial commit

---
+++ Source/SpaceDiner/SpaceDiner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceDiner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceDiner, "SpaceDiner" );
